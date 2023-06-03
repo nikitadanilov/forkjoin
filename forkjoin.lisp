2cab4e24417a52382b5e29526f06fd9e256cee28
Nikita Danilov <danilov@gmail.com>
2023-06-03 16:49:01 -0700

    Add NEXT and DONE call-backs.
    Signed-off-by: Nikita Danilov <danilov@gmail.com>

--- forkjoin.lisp
+++ forkjoin.lisp
@@ -13,14 +13,24 @@
 ;;;
 ;;; This computes FOO, BAR and BAZ in 3 separate threads and waits until all of them complete.
 ;;;
-;;; Wait can be separate:
+;;; Join can be separate:
 ;;; 
-;;; (LET ((GROUP (FJ:FORK-LAUNCH T ; T means 'propagate conditions to the waiter'.
+;;; (LET ((GROUP (FJ:FORK-LAUNCH (T) ; T means 'propagate conditions to the waiter'.
 ;;;                (FOO)
 ;;;                (BAR X)
 ;;;                (BAZ))))
-;;;   (COMPUTE-SOMETHING)          ; Compute concurrently with the forked functions.
-;;;   (FJ:WAIT GROUP))             ; Wait for FOO, BAR and BAZ completion.
+;;;   (COMPUTE-SOMETHING)            ; Compute concurrently with the forked functions.
+;;;   (FJ:WAIT GROUP))               ; Wait for FOO, BAR and BAZ completion.
+;;;
+;;; Instead of synchronous join, call-backs can be specified for a group. NEXT
+;;; call-back is called with 2 parameters (the group and the fork), when a
+;;; forked thread completes in the group and DONE call-back is called with the
+;;; group as the parameter when all forks complete:
+;;;
+;;; (FJ:FORK-LAUNCH (T #'NEXT #'DONE)
+;;;   (FOO)
+;;;   (BAR X)
+;;;   (BAZ))
 ;;;
 ;;; Conditions, errors, signals.
 ;;;
@@ -58,9 +68,9 @@
    ; List of propagated conditions, not yet re-signalled.
    (sigs  :initform ())
    ; Optional call-back invoked on each forked thread termination.
-   (next  :initform (constantly nil))
+   (next  :initform (constantly nil) :initarg :next)
    ; Optional call-back invoked on the group termination.
-   (done  :initform (constantly nil))))
+   (done  :initform (constantly nil) :initarg :done)))
 
 (defun fork-name (group function) ; Underlying thread name.
   (format nil "fork-~a-~a" group function))
@@ -91,8 +101,8 @@
 			   :name (fork-name group function)))
 	(push f forks)))))
 
-(defun make-group (prop)
-  (make-instance 'group :prop prop))
+(defun make-group (prop &key next done)
+  (make-instance 'group :prop prop :next next :done done))
 
 (defun handle (group) ; Handle propagated conditions.
   (with-slots (prop sigs) group
@@ -107,13 +117,13 @@
 	  (condition-wait wait lock))))))
 
 ; Create and return a group of forked functions.
-(defmacro fork-launch (prop &body body) 
+(defmacro fork-launch ((prop &optional (next (constantly t)) (done (constantly t))) &body body) 
   (let ((g (gensym)))
-    `(let ((,g (make-group ,prop)))
+    `(let ((,g (make-group ,prop :next ,next :done ,done)))
        ,@(loop for f in body collect `(make-fork ,g #'(lambda () ,f)))
        ,g)))
 
 (defmacro fork (&body body) ; Fork and wait.
-  `(wait (fork-launch t ,@body)))
+  `(wait (fork-launch (t) ,@body)))
 
 
