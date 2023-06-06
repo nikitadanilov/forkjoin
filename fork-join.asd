1e395d70ea845cb9347ab0c3c2cdea8822fc5e21
Nikita Danilov <danilov@gmail.com>
2023-06-06 13:00:56 -0700

    Add unit tests (with #:parachute).
    Signed-off-by: Nikita Danilov <danilov@gmail.com>

--- fork-join.asd
+++ fork-join.asd
@@ -1,3 +1,5 @@
+;;;; -*- Mode: LISP; Syntax: Ansi-Common-Lisp; Base: 10; -*-
+
 (asdf:defsystem "fork-join"
   :description "Fork-join style concurrency."
   :author "Nikita Danilov <danilov@gmail.com>"
@@ -10,3 +12,16 @@
   :serial t
   :depends-on (#:bordeaux-threads)
   :components ((:file "fork-join")))
+
+(asdf:defsystem "fork-join/tests"
+  :description "Unit tests for fork-join."
+  :author "Nikita Danilov <danilov@gmail.com>"
+  :maintainer "Nikita Danilov <danilov@gmail.com>"
+  :homepage "https://github.com/nikitadanilov/forkjoin"
+  :bug-tracker "https://github.com/nikitadanilov/forkjoin/issues"
+  :source-control (:git "https://github.com/nikitadanilov/forkjoin.git")
+  :license "GPL2"
+  :version "0.0.1"
+  :serial t
+  :depends-on (#:fork-join #:parachute)
+  :components ((:file "tests")))
---
+++ tests.lisp
@@ -0,0 +1,159 @@
+;;;; -*- Mode: LISP; Syntax: Ansi-Common-Lisp; Base: 10; -*-
+
+(defpackage :fork-join/tests
+  (:documentation "Fork-join unit tests")
+  (:nicknames :fj/t)
+  (:use #:parachute #:fork-join #:common-lisp #:bordeaux-threads))
+
+(in-package #:fork-join/tests)
+
+(define-test all)
+
+(define-test fork/base
+  :parent all)
+
+(define-test fork/simple
+  :parent fork/base
+  (true (progn (fork) t))
+  (true (progn (fork 0) t))
+  (true (progn (fork 0 1) t))
+  (true (progn (fork 0 1 2) t)))
+
+(define-test fork/time
+  :parent fork/base
+  :time-limit 1.2
+  (true (progn (fork
+		 (sleep 1)
+		 (sleep 1)
+		 (sleep 1))
+	       t)))
+
+(define-test fork/order
+  :parent fork/base
+  (is equal (let (out) (fork
+			 (progn (sleep 1) (push 1 out))
+			 (progn (sleep 2) (push 2 out))
+			 (progn (sleep 3) (push 3 out)))
+	      out)
+      (reverse '(1 2 3))))
+  
+(define-test fork/mutex
+  :parent fork/base
+  (is equal (sort (let (out
+			(guard (bordeaux-threads:make-lock)))
+		    (fork
+		      (bordeaux-threads:with-lock-held (guard) (push 1 out))
+		      (bordeaux-threads:with-lock-held (guard) (push 2 out))
+		      (bordeaux-threads:with-lock-held (guard) (push 3 out)))
+		    out)
+		  #'<)
+      '(1 2 3)))
+
+(define-test fork/tine
+  :parent fork/base
+  (is equal (sort (let (out
+			(guard (bordeaux-threads:make-lock))
+			(fork (make-fork t)))
+		    (loop for i in '(1 2 3 4 5 6 7 8 9) do
+		      (let ((i i))
+			(add-tine fork
+				     (make-tine
+				      #'(lambda ()
+					  (progn
+					    (sleep (random 2.0))
+					    (bordeaux-threads:with-lock-held (guard)
+					      (push i out))))))))
+		    (wait fork)
+		    out)
+		  #'<)
+      '(1 2 3 4 5 6 7 8 9)))
+
+(define-test fork/next-done
+  :parent fork/base
+  (is = (let* ((guard (bordeaux-threads:make-lock))
+	       (c 0)
+	       (cb #'(lambda (&rest args)
+		       (declare (ignore args))
+		       (bordeaux-threads:with-lock-held (guard) (incf c))))
+	       (fork (make-fork t :next cb :done cb)))
+	  (fork-with fork 1 2 3 4)
+	  (wait fork)
+	  c)
+      5))
+
+(define-test fork/cond
+  :parent all)
+
+(define-test fork/propagate
+  :parent fork/cond
+  (true (handler-case (fork
+			0
+			(signal 'error)
+			2)
+	  (error () t))))
+
+(define-test fork/propagate-through
+  :parent fork/cond
+  (true (handler-case (fork
+			0
+			(fork
+			  1.1
+			  (signal 'error)
+			  1.2)
+			2)
+	  (error () t))))
+
+(define-test fork/no-propagate
+  :parent fork/cond
+  (is equal t (let ((fork (make-fork nil)))
+		(fork-with fork
+		  (sleep 0.1)
+		  (signal 'error))
+		t)))
+
+(define-test fork/terminate
+  :parent all)
+
+(define-test fork/kill
+  :parent fork/terminate
+  :time-limit 0.5
+  (true (let ((fork (make-fork t)))
+	  (fork-with fork
+	    (loop (sleep 1))
+	    (kill fork))
+	  t)))
+
+(define-test fork/kill-up
+  :parent fork/terminate
+  :time-limit 0.5
+  (true (let ((fork (make-fork t)))
+	  (fork-with fork
+	    (loop (sleep 1))
+	    (let ((subfork (make-fork t)))
+	      (fork-with subfork
+		(loop (sleep 1))
+		(kill subfork))))
+	  t)))
+
+(define-test fork/kill-down
+  :parent fork/terminate
+  :time-limit 0.5
+  (true (let ((fork (make-fork t)))
+	  (fork-with fork
+	    (loop (sleep 1))
+	    (let ((subfork (make-fork t)))
+	      (fork-with subfork
+		(loop (sleep 1))
+		(kill fork))))
+	  t)))
+
+(define-test fork/timeout
+  :parent fork/terminate
+  :time-limit 1.0
+  (true (let ((fork (make-fork t)))
+	  (fork-timeout fork 0.5)
+	  (fork-with fork
+	    (loop (sleep 1)))
+	  t)))
+  
+(test 'all)
